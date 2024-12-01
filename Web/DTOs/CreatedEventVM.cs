6cabc203fe7cea283e2619c3ba5febebc17a36f7
Артём Принцев <printsev.artyom@yandex.ru>
2024-12-01 13:55:36 +0700

    feat: add event method

---
+++ Web/DTOs/CreatedEventVM.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Web.DTOs
+{
+    /// <summary>
+    /// Объект для создания мероприятия
+    /// </summary>
+    public class CreatedEventVM
+    {
+        /// <summary>
+        /// Название
+        /// </summary>
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Название обязательно")]
+        public string Name { get; set; } = null!;
+        /// <summary>
+        /// Дата и время 
+        /// </summary>
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Дата и время обязательны")]
+        public DateTime DateTime { get; set; }
+        /// <summary>
+        /// Описание
+        /// </summary>
+        public string? Description { get; set; }
+    }
+}
